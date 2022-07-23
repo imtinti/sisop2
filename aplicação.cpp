8393f59a55b498826ee0b130dcf7cdc19c7880d3
iuri <47192296+imtinti@users.noreply.github.com>
2022-07-23 11:11:18 -0300

    criado escopo das funcoes iniciais

--- aplicação.cpp
+++ aplicação.cpp
@@ -1,11 +1,56 @@
+#include "global.h"a
+
+
+//cria o diretorio "sync_dir" e inicia atividades de sincronização
+get_sync_dir() {
+
+
+}; 
+
+
+
+// lista os arquivos salvos no diretorio "sync_dir"
+list_client(){
+
+
+}; 
+
+
+
+// lista os arquivos salvos no servidor associado ao usuário
+list_server() {
+
+
+};
+
+
+// envia o arquivo filename.ext para o servidor associado ao usuário
+upload(path / filename.ext) {
+
+
+}; 
+
+download(path / filename.ext); //faz uma "copia não sincronizada" do arquivo filename.ext do servidor para\n 
+							   //o diretório local (de onde o servidor foi chamado)
+
+delete(char* filename.ext);
+
+
+exit();
+
 main() {
 
 
-	iniciarSessao();
+	//	iniciarSessao();
+	//Um cliente deve poder estabelecer uma sessão com o servidor via linha de comando utilizando :
+	//# . / myClient <username> <server_ip_address> <port>
+	//Equivalente a //	loginUsuario();
 
-	loginUsuario();
+	bool iniciarSessao(char* username, char* server_ip_address, int port);//retorna true em caso de sucesso de conexão e false caso contrario
 
-	loginServer();
+//	loginServer();
+
+
 
 
 
