b7d0d51d323d1e99822f46010896d92cd735a938
iuri <47192296+imtinti@users.noreply.github.com>
2022-07-24 15:47:38 -0300

    funcoes movidas de aplicaçao.pp para a classe myCliente

--- aplicação.cpp
+++ aplicação.cpp
@@ -1,41 +1,5 @@
 #include "global.h"
 
-// check if the given string is a numeric string or not
-bool chkNumber(const string& str) {
-	return !str.empty() &&
-		(str.find_first_not_of("[0123456789]") == std::string::npos);
-}
-
-// Function to split string str using given delimiter
-vector<string> split(const string& str, char delim) {
-	auto i = 0;
-	vector<string> list;
-	auto pos = str.find(delim);
-	while (pos != string::npos) {
-		list.push_back(str.substr(i, pos - i));
-		i = ++pos;
-		pos = str.find(delim, pos);
-	}
-	list.push_back(str.substr(i, str.length()));
-	return list;
-}
-
-//Fonte: https://www.tutorialspoint.com/validate-ip-address-in-cplusplus
-// Function to validate an IP address
-bool validateIP(string ip) {
-	// split the string into tokens
-	vector<string> slist = split(ip, '.');
-	// if token size is not equal to four
-	if (slist.size() != 4)
-		return false;
-	for (string str : slist) {
-		// check that string is number, positive, and range
-		if (!chkNumber(str) || stoi(str) < 0 || stoi(str) > 255)
-			return false;
-	}
-	return true;
-}
-
 //cria o diretorio "sync_dir" e inicia atividades de sincronização
 bool get_sync_dir() {
 
@@ -85,79 +49,12 @@ bool exit() {
 
 
 
-//funcao que retorna um valor inteiro único para uma dada string. 
-// retorna um código pra cada entrada do usuário. 
-//retirado de: https://stackoverflow.com/questions/16388510/evaluate-a-string-with-a-switch-in-c
-constexpr unsigned int str2int(const char* str, int h = 0)
-{
-	return !str[h] ? 5381 : (str2int(str, h + 1) * 33) ^ str[h];
-}
-
-
-//testa se é um numero inteiro
-bool validatePort(string argumento) {
-
-	if ((argumento.find_first_not_of("0123456789") == string::npos) = true)
-		return true;
-	else
-		return false;
-
-}
-
-
-bool trataEntrada(int codIn, char* argumento) {
-
-	//./myClient <username>[0] <server_ip_address>[1] <port>[2]
-
-	switch (codIn)
-	{
-		case 0:
-
-			return true;
-			break;
-
-		case 1:
-
-			if (validateIP(argumento))
-				return true;
-			else
-				return false;
-			break;
-
-		case 2:
-
-			if (validatePort(argumento))
-				return true;
-			else
-				return false;
-
-		default:
-			return false;
-	}
-
-
-};
-
-bool qntdArgumentosOK(int nroParametrosEntrada) {
-
-	if (nroParametrosEntrada = 3)
-		return true;
-	else
-		return false;
-}
-
-
 using namespace std;
-int main(int argc, char* argv[]) {
+int main() {
 //sobre atributos de entrada na função main:: https://docs.microsoft.com/pt-br/cpp/cpp/main-function-command-line-args?view=msvc-170
 
 
-	//Um cliente deve poder estabelecer uma sessão com o servidor via linha de comando utilizando :
-	//# ./myClient <username> <server_ip_address> <port>
-	//Equivalente a //	loginUsuario();
-	char* username;
-	char* server_ip_address;
-	char* port;
+
 
 	//testa se a quantidade de parâmetros de entrada é igual a 3
 	if (!qntdArgumentosOK(argc))
@@ -167,20 +64,20 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	 if(trataEntrada(inputUSERNAME, argv[0]) 
+	 if(trataEntrada(inputUSERNAME, argv[0]) //testa se é um nome válido
 		username = argv[0];//captura o primeiro argumenrto de entrada do usuário
 	 else
 		 printf("Nome de usuário inválido\n");
 		 return 0;
 
 
-	if(trataEntrada(inputIP, argv[1]))
+	if(trataEntrada(inputIP, argv[1]))//testa se é um IP válido
 		server_ip_address = argv[1] //captura o segundo argumento de entrada do usuário
 	else
 		printf("Ip inválido\n");
 		return 0;
 
-	if (trataEntrada(inputPORT, arqv[2]))
+	if (trataEntrada(inputPORT, arqv[2]))//testa se é uma porta válida (algum valor inteiro maior q zero)
 		port = arqv[2]; //captura o terceiro argumento de entrada do usuário
 	else
 		printf("Port inválido\n");
